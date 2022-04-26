f419a419ed5d160b20b61bc303d7b9bc73208d4b
MrZeRo2000 <MrZeRo2000@mail.ru>
2022-04-26 18:49:25 +0300

    amended NetworkUtils with internet connection monitoring, version 1.3.13

--- library/src/main/java/com/romanpulov/library/common/network/NetworkUtils.java
+++ library/src/main/java/com/romanpulov/library/common/network/NetworkUtils.java
@@ -2,6 +2,10 @@ package com.romanpulov.library.common.network;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+
+import androidx.annotation.NonNull;
 
 import java.io.IOException;
 import java.net.InetAddress;
@@ -13,6 +17,48 @@ import java.net.InetAddress;
 
 public final class NetworkUtils {
     private static final int HOST_CHECK_TIMEOUT = 30;
+    private static final int CAPABILITIES_CHANGE_DELAY = 5000;
+
+    public interface OnInternetAvailableListener {
+        void onInternetAvailable (@NonNull NetworkCapabilities networkCapabilities);
+    }
+
+    public static class ConnectivityMonitor extends ConnectivityManager.NetworkCallback {
+        long mLastAvailableTime;
+
+        OnInternetAvailableListener mListener;
+
+        public void registerOnInternetAvailableListener(OnInternetAvailableListener mListener) {
+            this.mListener = mListener;
+        }
+
+        @Override
+        public void onAvailable(@NonNull Network network) {
+            super.onAvailable(network);
+            mLastAvailableTime = System.currentTimeMillis();
+        }
+
+        @Override
+        public void onCapabilitiesChanged(@NonNull Network network, @NonNull NetworkCapabilities networkCapabilities) {
+            super.onCapabilitiesChanged(network, networkCapabilities);
+            long currentTime = System.currentTimeMillis();
+
+            if (mListener != null &&
+                    networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_ROAMING) &&
+                    networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED) &&
+                    mLastAvailableTime > 0 &&
+                    (currentTime - mLastAvailableTime) < CAPABILITIES_CHANGE_DELAY
+            ) {
+                mListener.onInternetAvailable(networkCapabilities);
+            }
+        }
+
+        @Override
+        public void onLost(@NonNull Network network) {
+            super.onLost(network);
+            mLastAvailableTime = 0;
+        }
+    }
 
     /**
      * Checks if network is available
